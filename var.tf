348abb5977a6e0b59918155c8034920670b8e4d3
naresh26git <111833906+naresh26git@users.noreply.github.com>
2023-12-19 13:18:44 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -53,7 +53,7 @@ variable "az_sub_pvt1" {
   default     = "eu-central-1a" 
 }
 # azpvt-2
-variable "az_sub_pvt4" {
+variable "az_sub_pvt2" {
   type        = string 
   description = "subnet"
   default     = "eu-central-1b"
